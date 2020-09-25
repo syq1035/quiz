53a5177cb952e63ca8fcb1aa23537e48e594fd18
syq <1035394794@qq.com>
2020-09-25 17:45:48 +0800

    [syq] add home page

--- front-end/src/pages/home/index.js
+++ front-end/src/pages/home/index.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Home = () => {
-  return <div className="main">This is Home page</div>;
-};
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      goods: [
+        { name: "可乐", price: 2, unit: "瓶" },
+        { name: "可乐", price: 2, unit: "瓶" },
+        { name: "可乐", price: 2, unit: "瓶" },
+        { name: "可乐", price: 2, unit: "瓶" },
+        { name: "可乐", price: 2, unit: "瓶" }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="home-main">
+        {this.state.goods.length > 0 ? (
+          this.state.goods.map((product, index) => (
+            <div className="goods-item" key={index}>
+              <img alt="goods" />
+              <p>{product.name}</p>
+              <p>
+                单价：{product.price}元 / {product.unit}
+              </p>
+            </div>
+          ))
+        ) : (
+          <p>暂无商品</p>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Home;
